84bc6a42aefcfce5f8bf572b4aea0fd8db9cd718
Mr Josh <dotmrjosh@sparklapse.com>
2024-03-11 09:47:09 +0000

    Created breakfast cli creator

--- create-breakfast/bin.js
+++ create-breakfast/bin.js
@@ -1,3 +1,42 @@
 #!/usr/bin/env node
 
-console.log("Placeholder");
+import degit from "degit";
+import { resolve } from "node:path";
+import { intro, spinner, outro, cancel, text } from "@clack/prompts";
+
+intro("Lets start making something yummy...");
+
+const currentDirectory = process.cwd();
+const directory = await text({
+  message: "Where should we get cooking?",
+  initialValue: "./breakfast-plugin",
+  validate: (input) => {
+    if (input.length < 1) return "Please enter a directory";
+  },
+});
+
+const destination = resolve(
+  directory.startsWith("/") ? "" : currentDirectory,
+  directory
+);
+
+const { start, stop } = spinner();
+
+start("Pulling in the template...");
+
+try {
+  const emitter = degit("github:sparklapse/breakfast-plugins/template", {
+    force: true,
+    verbose: true,
+  });
+
+  await emitter.clone(destination);
+} catch (err) {
+  cancel("Something went wrong, please try again.");
+  console.error(err);
+  process.exit(1);
+}
+
+stop("Clone complete!");
+
+outro("You're ready to get cooking, Good luck!");
